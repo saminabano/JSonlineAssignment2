50602540011d7f5a0747d1a6538de25fc337fd6b
saminabano <saminaasif33@gmail.com>
2023-04-11 22:28:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+//Question #1
+ //let input_1=+prompt("enter NO 1");
+ //let input_2=+prompt("enter No 2");
+
+ //function parent_function(value_1){
+     //return function child_functon(value_2){
+     //    console.log(value_1 + value_2);
+         
+   //  }
+ //}
+
+ //let function_1 = parent_function(input_1);
+ //let function_2 = function_1(input_2);
+
+//----------------------------------------------
+//Question#2
+
+// let arr = [5,9,,67,85,91,96];
+
+// let no_to_search = 5;
+
+// let i = 0;
+
+// function recursion(array){
+//     if (no_to_search === array[i]){
+//        console.log("true");
+//     }
+//     else if(i < array.length-1){ 
+//         i = i+1; //i =1;
+//         recursion(array);
+//     }
+//     else {
+//        console.log("false");
+//     }
+// }
+// recursion(arr);
+
+
+// function recursion(array){
+//     if (no_to_search === array[i]) {
+//         return true;
+//     }
+//     else if(i < array.length-1){
+//         i = i+1;
+//         recursion();
+//        }
+//     else{ 
+//         return;
+//     }
+// }
+
+// //=========calling function
+// recursion(arr);
+
+// //=========printing true and flase
+// if (recursion(arr)==true){
+//     console.log("true");
+// }
+// else{
+//     console.log("flase");
+// }
+
+
+
+
+
+
+//Question#3
+
+// let input_text = prompt("enter content:")
+// let para = document.createElement("p");
+// let parent = document.getElementsByTagName("html")[0];
+// // console.log(parent);
+// function para_work(string) {
+//     para.innerHTML = string;
+//     parent.appendChild(para);
+//     parent.insertAdjacentElement("afterbegin", para);
+// }
+
+// para_work(input_text)
+
+//=================================================================
+
+//Question#4
+
+
+//Question # 5
+//function changeBackgroundColor(element, newColor) {
+//    element.style.backgroundColor = newColor;
+  //}
+  //const myDiv = document.getElementById('myDiv');
+//changeBackgroundColor(myDiv, 'red');
+
